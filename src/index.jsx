74cc0a3ecf83fa5b4b20007f79926f60996ffe97
Ryan Woods <contact@ryanofwoods.com>
2021-04-05 15:47:56 +0200

    update the index to have an initial state and default routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -7,20 +8,37 @@ import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
+// internal modules
 import '../assets/stylesheets/application.scss';
+import carsIndex from './containers/cars_index';
+
+// reducers
+import carsReducer from './reducers/cars_reducer';
+
+const garageName = 'LeGarage'; // prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const initialState = {
+  garage: garageName,
+  cars: [
+    { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+    { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+    { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+    { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+  ]
+};
 
 const reducers = combineReducers({
-  // key: reducer
+  garage: (state = null) => state,
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <Router history={history}>
       <Switch>
-        TODO
+        <Route path="/:garage" component={carsIndex} />
       </Switch>
     </Router>
   </Provider>,
---
+++ src/reducers/cars_reducer.js
@@ -0,0 +1,3 @@
+export default function carsReducer(state = null, action) {
+  
+}
